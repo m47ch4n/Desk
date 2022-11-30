6261212efaaf06079b283231776e572d3d64b56e
Shoya Matsumoto <m47ch4n@gmail.com>
2022-11-30 19:56:57 +0900

    Implement sign out

--- crates/plugins/desk-firebase-auth/src/lib.rs
+++ crates/plugins/desk-firebase-auth/src/lib.rs
@@ -1,7 +1,10 @@
-
 use bevy::prelude::*;
 use wasm_bindgen::prelude::*;
-
+use desk_window::{
+    ctx::Ctx,
+    widget::{Widget, WidgetId},
+    window::{DefaultWindow, Window},
+};
 
 #[wasm_bindgen]
 extern "C" {
@@ -10,16 +13,32 @@ extern "C" {
 
     #[wasm_bindgen]
     fn sign_in_with_github();
+
+    #[wasm_bindgen]
+    fn sign_out();
 }
 
-use desk_window::{
-    ctx::Ctx,
-    widget::{Widget, WidgetId},
-    window::{DefaultWindow, Window},
-};
+#[derive(Debug, Clone)]
+pub struct FirebaseUser {
+    pub name: String,
+    pub email: String
+}
 
-use once_cell::sync::OnceCell;
-pub static SIGNED: OnceCell<bool> = OnceCell::new();
+use parking_lot::Mutex;
+static SIGNED_USER: Mutex<Option<FirebaseUser>> = Mutex::new(None);
+impl FirebaseUser {
+    pub fn get() -> Option<FirebaseUser> {
+        SIGNED_USER.lock().clone()
+    }
+
+    fn set(name: String, email: String) {
+        SIGNED_USER.lock().replace(FirebaseUser { name, email });
+    }
+
+    fn reset() {
+        SIGNED_USER.lock().take();
+    }
+}
 
 pub struct FirebaseAuthPlugin;
 
@@ -40,7 +59,13 @@ struct FirebaseAuthWidget;
 impl Widget<egui::Context> for FirebaseAuthWidget {
     fn render(&mut self, ctx: &mut Ctx<egui::Context>) {
         egui::Window::new("Sign in").show(ctx.backend, |ui| {
-            // let window = web_sys::window().unwrap();
+            if let Some(user) = FirebaseUser::get() {
+                ui.label(format!("Welcome {}", user.name));
+                if ui.button("Sign out").clicked() {
+                    sign_out();
+                }
+                return
+            }
             if ui.button("Sign in with Google").clicked() {
                 sign_in_with_google();
             }
@@ -52,7 +77,11 @@ impl Widget<egui::Context> for FirebaseAuthWidget {
 }
 
 #[wasm_bindgen]
-pub fn on_signed_in() {
-    SIGNED.set(true).expect("cannot set");
-    print!("Signed {}", SIGNED.get().unwrap());
+pub fn on_signed_in(email: &str, name: &str) {
+    FirebaseUser::set(name.to_string(), email.to_string());
+}
+
+#[wasm_bindgen]
+pub fn on_signed_out() {
+    FirebaseUser::reset();
 }
